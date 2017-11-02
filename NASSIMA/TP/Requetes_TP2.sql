4ed4c3176de764973f92a5be29e75a6a2f9253bb
gmenant <gab.menant@gmail.com>
2017-11-02 17:03:29 +0100

    Fin de journée
    TP2 en cours

---
+++ NASSIMA/TP/Requetes_TP2.sql
@@ -0,0 +1,17 @@
+1
+INSERT INTO `F`(`NumF`,`NomF`,`Statut`,`VilleF`)
+VALUES ('43','Hachette','client','Paris');
+
+2
+DELETE FROM P WHERE couleur="noir" AND NumP BETWEEN '100' AND '1999';
+
+3
+UPDATE P SET ville='TOULOUSE' WHERE NumP='3';
+
+4
+SELECT NumU,NomU,VilleU FROM U ;
+
+5
+SELECT NumU,NomU,VilleU FROM U WHERE ville='Paris';
+
+6
